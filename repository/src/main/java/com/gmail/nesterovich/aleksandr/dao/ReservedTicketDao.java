aa7afc649ab24bb9ddf2ada824b27b0b68033899
Aleksandr Nesterovich <nesterovich.aleksandr.a@gmail.com>
2019-02-28 19:10:40 +0300

    added ReservedTicketDao

---
+++ repository/src/main/java/com/gmail/nesterovich/aleksandr/dao/ReservedTicketDao.java
@@ -0,0 +1,6 @@
+package com.gmail.nesterovich.aleksandr.dao;
+
+import com.gmail.nesterovich.aleksandr.model.ReservedTicket;
+
+public interface ReservedTicketDao extends BaseDao<ReservedTicket, String> {
+}
