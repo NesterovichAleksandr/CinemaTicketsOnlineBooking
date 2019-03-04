e63f3cc47bb6f275d158e717591a71b845c0275a
Aleksandr Nesterovich <nesterovich.aleksandr.a@gmail.com>
2019-03-04 19:00:53 +0300

    added GenreDto

---
+++ service/src/main/java/com/gmail/nesterovich/aleksandr/model/GenreDto.java
@@ -0,0 +1,26 @@
+package com.gmail.nesterovich.aleksandr.model;
+
+public class GenreDto {
+
+    private Long id;
+    private String genre;
+
+    public GenreDto() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+}
